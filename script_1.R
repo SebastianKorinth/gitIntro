c6534e1c315fa39b7c0e3f53fd7dac7c02d2a1a0
Sebastian Korinth <s.korinth@dipf.de>
2025-10-23 13:36:01 +0200

    added some colors

--- script_1.R
+++ script_1.R
@@ -3,7 +3,9 @@ library(ggplot2)
 a <- 1:100
 b <- runif(100, min = 1, max = 999)
 ab.df <- data.frame(a,b)
+ab.df$color <- "green"
+ab.df$color[1:50] <- "red"
 
-ggplot(data = ab.df, aes(x = a, y = b)) +
-  geom_line() +
+ggplot(data = ab.df, aes(x = a, y = b, group = color)) +
+  geom_line(aes(color = color)) +
   geom_point()
