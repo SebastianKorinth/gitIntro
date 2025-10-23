145356a92f926a8a792f07bba6aa06d425297585
Sebastian Korinth <s.korinth@dipf.de>
2025-10-23 14:28:28 +0200

    added some comments

--- script_1.R
+++ script_1.R
@@ -1,11 +1,14 @@
+# load library
 library(ggplot2)
 
+# create some data
 a <- 1:100
 b <- runif(100, min = 1, max = 999)
 ab.df <- data.frame(a,b)
 ab.df$color <- "green"
 ab.df$color[1:50] <- "red"
 
+# make a line plot
 ggplot(data = ab.df, aes(x = a, y = b, group = color)) +
   geom_line(aes(color = color)) +
   geom_point(aes(size = b))
